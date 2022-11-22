9e9d24f6979385e72d07c61f6c48393464ac0706
rifathossain82 <officialrifat82@gmail.com>
2022-11-22 18:08:14 +0600

    push notification 80 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,61 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_sslcommerz/model/SSLCAdditionalInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCCustomerInfoInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCEMITransactionInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
-import 'package:flutter_sslcommerz/model/SSLCShipmentInfoInitializer.dart';
-import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
-import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
-import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
-import 'package:flutter_sslcommerz/model/sslproductinitilizer/General.dart';
-import 'package:flutter_sslcommerz/model/sslproductinitilizer/SSLCProductInitializer.dart';
-import 'package:flutter_sslcommerz/sslcommerz.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'package:hive_flutter/adapters.dart';
 import 'package:pay_app/Hive/hive_homePage.dart';
-
+import 'package:workmanager/workmanager.dart';
+import 'package:http/http.dart' as http;
 enum SdkType { TESTBOX, LIVE }
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    if(task=='uniquekey'){
+      // do the task in Backend for how and when to send notification 
+      var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/2')); 
+      Map<String,dynamic> resData = json.decode(response.body); 
+      print(resData['title']);
+      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+      const AndroidNotificationDetails androidNotificationChannelSpecifics = 
+      AndroidNotificationDetails("your channel id", 'your channel name', 
+   importance: Importance.max, priority: Priority.high, 
+      showWhen: false, 
+      );
+    
+    const   NotificationDetails platformChannelSpecifics =  NotificationDetails(
+        android: androidNotificationChannelSpecifics
+      );
+      await flutterLocalNotificationsPlugin.show(
+        0, resData['data']['title'], resData['data']['body'],
+         platformChannelSpecifics, 
+         payload: 'item x');
+         return Future.value(true); 
 
+
+    }
+    return Future.value(true);
+  });
+}
 Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized(); 
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings  initializationSettingsAndroid = AndroidInitializationSettings(
+    '@mipmap/ic_launcher'
+  );
+  final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+   flutterLocalNotificationsPlugin.initialize(initializationSettings, 
+  );
+  
+  Workmanager().initialize(callbackDispatcher, 
+  isInDebugMode: true); 
+  Workmanager().registerPeriodicTask("2","uniquekey", frequency: Duration(seconds: 5), 
+  
+  inputData:{"title":'title'} ); 
+
+
+  
   await Hive.initFlutter(); 
   await Hive.openBox('vocabulary'); 
 
@@ -30,6 +67,11 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+ Future<void> selectNotification(String payload) async{
+if(payload != null){
+
+}
+ }
 class _MyAppState extends State<MyApp> {
   var _key = GlobalKey<FormState>();
   dynamic formData = {};
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import flutter_local_notifications
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
